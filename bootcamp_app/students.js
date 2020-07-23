e35bfb4b33fba0752862ccb41b5b86d4130e1c64
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-07-23 04:44:30 +0000

    protect against sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,18 +7,25 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+const numOfResults = process.argv[3];
+const values = [`%${cohortName}%`, numOfResults]
+
+const text = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-}).catch(err => console.error('query error', err.stack));
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(text, values)
+    .then(res => {
+      res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+      })
+    })
+    .catch(err => console.error('query error', err.stack));
 
 
 /*When we make the query from our JavaScript application, the results come back as JavaScript objects.
