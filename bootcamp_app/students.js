6b90e5605e2156a883afc31a1f9cdf52295c610e
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-07-23 03:45:35 +0000

    add node postgres

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,3 +7,32 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
+
+
+/*When we make the query from our JavaScript application, the results come back as JavaScript objects.
+That means that once the .then(res => {}) gets executed, we're not dealing with SQL or the database any more,
+we're just dealing with JavaScript objects
+*/
+
+// pool.query(`
+// SELECT id, name, cohort_id
+// FROM students
+// LIMIT 5;
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
+//   })
+// });
